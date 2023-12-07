21972299c799b639cba7d13e90739c55d7aa6c94
Mateusz Kolpa <mateusz.kolpa@wp.pl>
2023-12-07 22:29:46 +0100

    Add some basic server functionality

--- src/client/main.c
+++ src/client/main.c
@@ -1,8 +1,57 @@
 #include "game/game.h"
 
+#include <netdb.h>
+#include <netinet/in.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+
+void init_sockaddr(
+	struct sockaddr_in *name, const char *hostname, uint16_t port
+) {
+	struct hostent *hostinfo;
+
+	name->sin_family = AF_INET;
+	name->sin_port   = htons(port);
+	hostinfo         = gethostbyname(hostname);
+	if (hostinfo == NULL) {
+		fprintf(stderr, "Unknown host %s.\n", hostname);
+		exit(EXIT_FAILURE);
+	}
+	name->sin_addr = *(struct in_addr *) hostinfo->h_addr;
+}
 
 int main(int argc, const char *args[]) {
+	int                sock_fd = socket(PF_INET, SOCK_DGRAM, 0);
+	struct sockaddr_in server_addr;
+	init_sockaddr(&server_addr, "localhost", 7878);
+
+	const char *client_hello = "Hello from client!";
+	sendto(
+		sock_fd,
+		client_hello,
+		strlen(client_hello),
+		MSG_CONFIRM,
+		(const struct sockaddr *) &server_addr,
+		sizeof(server_addr)
+	);
+
+	socklen_t len;
+	char      buffer[1024];
+	int       n = recvfrom(
+        sock_fd,
+        buffer,
+        sizeof(buffer) / sizeof(buffer[0]),
+        MSG_WAITALL,
+        (struct sockaddr *) &server_addr,
+        &len
+    );
+
+	buffer[n] = '\0';
+	printf("Server says: %s\n", buffer);
+
+	// return 0;
 	Game game;
 
 	if (!GameInit(&game, argc, args)) {
--- src/server/main.c
+++ src/server/main.c
@@ -1,5 +1,89 @@
+#include <errno.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
+#include <unistd.h>
 
-int main() { int sock_fd = socket(AF_INET, SOCK_DGRAM, 0); }
+#define PORT 8080
+
+int main() {
+	int                sock;
+	struct sockaddr_in server_addr;
+
+	if ((sock = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
+		switch (errno) {
+		case EPROTONOSUPPORT:
+			fprintf(
+				stderr,
+				"The protocol or style is not supported by the "
+				"namespace specified."
+			);
+			break;
+		case EMFILE:
+			fprintf(
+				stderr,
+				"The process already has too many file descriptors open."
+			);
+			break;
+		case ENFILE:
+			fprintf(
+				stderr, "The system already has too many file descriptors open."
+			);
+			break;
+		case EACCES:
+			fprintf(
+				stderr,
+				"The process does not have the privilege to create a socket of "
+				"the specified style or protocol."
+			);
+			break;
+		case ENOBUFS:
+			fprintf(stderr, "The system ran out of internal buffer space.");
+			break;
+		}
+		perror("socket");
+		exit(EXIT_FAILURE);
+	}
+
+	server_addr.sin_family      = AF_INET;
+	server_addr.sin_port        = htons(7878);
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	if (bind(sock, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
+		perror("bind");
+		exit(EXIT_FAILURE);
+	}
+
+	socklen_t len;
+	int       n;
+
+	struct sockaddr_in client_addr;
+	len = sizeof(client_addr);
+	char buffer[1024];
+
+	n = recvfrom(
+		sock,
+		buffer,
+		sizeof(buffer) / sizeof(buffer[0]),
+		0,
+		(struct sockaddr *) &client_addr,
+		&len
+	);
+
+	buffer[n] = '\0';
+	printf("Client : %s\n", buffer);
+
+	const char *hello = "Hello from server!";
+	sendto(
+		sock,
+		hello,
+		strlen(hello),
+		MSG_CONFIRM,
+		(const struct sockaddr *) &client_addr,
+		len
+	);
+	printf("Hello message sent...\n");
+
+	close(sock);
+}
